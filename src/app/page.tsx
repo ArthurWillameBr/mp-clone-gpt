2c9c4b7b04fb477393a1c517c8bf7bbb01a0ed80
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-12 15:36:10 -0300

    feat: Add Sidebar component and update main page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,29 @@
-import { ReactElement } from "react";
+"use client";
+
+import { ReactElement, useState } from "react";
 import { KeyInstructions } from "./_components/key-instructions";
 import { ChatInput } from "./_components/chat-input";
+import { Sidebar } from "./_components/sidebar";
 
 function Home(): ReactElement {
+  const [ApiKey, setApiKey] = useState<string>("");
+
   return (
-    <main className="w-full h-screen p-10">
+    <div className="flex">
+      <Sidebar isVisible />
+ <main className="w-full h-screen p-10 flex flex-col justify-between">
       <h1 className="text-3xl pb-5 lg:text-[45px] font-bold text-gray text-center">
         CloneGPT
       </h1>
 
-      <KeyInstructions /> 
-      <ChatInput placeholder="Digite sua chave da API"/>
+      <KeyInstructions />
+      <ChatInput
+        onSubmitMessage={setApiKey}
+        placeholder="Digite sua chave da API"
+      />
     </main>
+    </div>
+   
   );
 }
 
