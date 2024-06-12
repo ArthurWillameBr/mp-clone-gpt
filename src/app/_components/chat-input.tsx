0afd7c52be7e5f8f2496a86cde0fff58dd0c9b90
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-12 15:35:33 -0300

    chore: Update ChatInput component to include image button for sending messages

--- src/app/_components/chat-input.tsx
+++ src/app/_components/chat-input.tsx
@@ -1,17 +1,31 @@
-"use client"
+"use client";
 
-import { useState } from "react";
+import React, { useState } from "react";
 import { Input } from "./ui/Input";
+import Image from "next/image";
 
 interface ChatInputProps {
   placeholder: string;
+  onSubmitMessage: (key: string) => void;
 }
 
-export const ChatInput = ({ placeholder}: ChatInputProps) => {
+export const ChatInput = ({ placeholder, onSubmitMessage }: ChatInputProps) => {
   const [inputValue, setInputValue] = useState("");
 
   function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
-    setInputValue(event.target.value)
+    setInputValue(event.target.value);
+  }
+
+  function handleKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === "Enter") {
+      onSubmitMessage(inputValue);
+      setInputValue("");
+    }
+  }
+
+  function handleClick() {
+    onSubmitMessage(inputValue);
+    setInputValue("");
   }
 
   return (
@@ -22,8 +36,20 @@ export const ChatInput = ({ placeholder}: ChatInputProps) => {
           value={inputValue}
           placeholder={placeholder}
           onChange={handleInputChange}
-          onKeyDown={() => {} }
+          onKeyDown={handleKeyDown}
         />
+
+        <div className="absolute flex items-center right-6 top-1/2 -translate-y-1/2 cursor-pointer ">
+          <button onClick={handleClick}>
+            <Image
+              src="/images/send-icon.svg"
+              width={24}
+              height={24}
+              alt="send"
+              quality={100}
+            />
+          </button>
+        </div>
       </div>
     </div>
   );
